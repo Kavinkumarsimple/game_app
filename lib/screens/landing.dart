f9792d4678129383517bdba1122dec6b83b8ca9a
Mohanraj Kavinkumar <mkavinkumarnaidu@gmail.com>
2024-08-02 11:53:33 +0530

    rows

--- lib/screens/landing.dart
+++ lib/screens/landing.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
 import 'package:flutter/material.dart';
 import 'package:game_app/shared/customAppBar.dart';
@@ -10,10 +10,44 @@ class LandingScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: MyAppBar(appBarText: 'Hello'),
-      body: CustomTitle(
-        title: 'Landing Screen',
-      ),
-    );
+        appBar: MyAppBar(appBarText: 'Hello'),
+        body: Container(
+            padding: EdgeInsets.all(20),
+            color: Colors.amber,
+            child: Container(
+              child: Row(
+                children: [
+                  Icon(
+                    Icons.settings,
+                    color: Colors.black,
+                    size: 30,
+                  ),
+                  Text(
+                    "Settings",
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 20,
+                      fontFamily: 'Oswald-Bold',
+                      fontWeight: FontWeight.bold,
+                    ),
+                  )
+                ],
+              ),
+            ))
+
+        // This is a icon Button
+        // Center(
+        //   child: IconButton(
+        //     icon: Image.asset(
+        //       'assets/images/people.png',
+        //       width: 20,
+        //       height: 20,
+        //     ),
+        //     onPressed: () {
+        //       print("Hi u clicked me");
+        //     },
+        //   ),
+        // ),
+        );
   }
 }
